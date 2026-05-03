b498a46b22446eae27af9a7e696d1f4f02cc9e96
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2026-05-03 13:30:03 +0100

    chore: add eslint.config.js - 'eslint  .' now works, and gives useful output

---
+++ eslint.config.js
@@ -0,0 +1,38 @@
+import tsparser from "@typescript-eslint/parser";
+import { defineConfig } from "eslint/config";
+import tseslint from 'typescript-eslint';
+import obsidianmd from "eslint-plugin-obsidianmd";
+
+export default defineConfig([
+    tseslint.configs.recommendedTypeChecked,
+    {
+        languageOptions: {
+            parserOptions: {
+                projectService: true,
+            },
+        },
+    },
+    ...obsidianmd.configs.recommended,
+    // Or include English locale files (JSON and TS/JS modules)
+    // ...obsidianmd.configs.recommendedWithLocalesEn,
+
+    {
+        files: ["src/*.ts"],
+        languageOptions: {
+            parser: tsparser,
+            // parserOptions: { project: "./tsconfig.json" },
+        },
+
+        // Optional project overrides
+        rules: {
+            "obsidianmd/ui/sentence-case": [
+                "warn",
+                {
+                    brands: ["YourBrand"],
+                    acronyms: ["OK"],
+                    enforceCamelCaseLower: true,
+                },
+            ],
+        },
+    },
+]);
