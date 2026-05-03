2da2f8771313394f2246671fe55c708c1ea51c0a
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2026-05-03 13:50:13 +0100

    comment: record how to fix setTimeout() lint error

--- src/plugin.ts
+++ src/plugin.ts
@@ -75,6 +75,7 @@ export class RevealActiveFileButtonPlugin extends Plugin {
       // @ts-ignore
       this.app.commands.executeCommandById('file-explorer:reveal-active-file');
       // Send the command twice like a double-click, to handle the frequent case where Obsidian fails to jump to the file
+      // TODO Set minAppVersion 0.15.0 then use activeWindow.setTimeout()
       setTimeout(() => {
         // @ts-ignore
         this.app.commands.executeCommandById('file-explorer:reveal-active-file');
